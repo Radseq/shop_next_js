35581e5eb25b52185d1255b4e253216dd95090c9
Radseq <radek.gom@gmail.com>
2023-02-27 12:38:30 +0100

    feat: move get product with score from api

--- src/server/products/product.ts
+++ src/server/products/product.ts
@@ -1,10 +1,6 @@
+import { CONFIG } from "@/config";
 import { prisma } from "prisma/prisma";
-
-export const getAllProducts = async () => {
-	const allProducts = await prisma.product.findMany();
-
-	return allProducts;
-};
+import { getProductScores, sumProductVotesByScore } from "../scores/score";
 
 export const getProductsByIds = async (productIds: number[]) => {
 	const product = await prisma.product.findMany({
@@ -16,4 +12,62 @@ export const getProductsByIds = async (productIds: number[]) => {
 	});
 
 	return product;
+};
+
+export const getProductKeyValueVotes = (
+	productId: number,
+	productScoresDb: ProductScore[]
+) => {
+	const votes: Record<number, number> = {};
+	for (
+		let scoreIndex = 0;
+		scoreIndex < CONFIG.SCORES_MAX_SIZE;
+		scoreIndex++
+	) {
+		votes[scoreIndex] = sumProductVotesByScore(
+			productId,
+			scoreIndex,
+			productScoresDb
+		);
+	}
+	return votes;
+};
+
+export const getProductById = async (productId: number) => {
+	const foundProductById = await prisma.product.findUnique({
+		where: { id: productId },
+	});
+
+	if (!foundProductById) return null;
+
+	const commentPageIndex = 1;
+
+	const [
+		productScoresDb,
+	] = await Promise.all([
+		getProductScores(productId),
+	]);
+
+	return {
+		product: foundProductById,
+		scores: getProductKeyValueVotes(productId, productScoresDb),
+	};
+};
+
+export const getProductsByName = async (productName: string) => {
+	const allSearchProductsByName = await prisma.product.findMany({
+		select: {
+			id: true,
+			name: true,
+			price: true,
+		},
+		where: {
+			name: {
+				contains: productName,
+			},
+		},
+		take: CONFIG.MAX_SEARCH_RESULT,
+	});
+
+	return allSearchProductsByName;
 };
---
+++ src/server/scores/score.ts
@@ -0,0 +1,21 @@
+import { prisma } from "prisma/prisma";
+
+export const getProductScores = async (productId: number) => {
+	const allProductScores = await prisma.productScore.findMany({
+		where: {
+			productId: productId,
+		},
+	});
+
+	return allProductScores;
+};
+
+export const sumProductVotesByScore = (
+	productId: number,
+	scoreIndex: number,
+	productScores: ProductScore[]
+) => {
+	return productScores.filter(
+		(score) => score.score == scoreIndex && score.productId == productId
+	).length;
+};
